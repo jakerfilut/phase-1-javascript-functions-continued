6770a779d97a7fdc5ca9ab459704a31e9fd7b562
Jake <jakerfilut@gmail.com>
2021-11-01 22:37:57 -0600

    commit tree 68ae84e593782f961d06b7bfc67de0749e0447b0

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+function saturdayFun(def1 = "roller-skate"){
+    return `This Saturday, I want to ${def1}!`
+}
+function mondayWork(def2 = "go to the office"){
+    return `This Monday, I will ${def2}.`
+}
+function wrapAdjective(symbol = "*"){
+    return function(def3 = "special"){
+        return `You are ${symbol}${def3}${symbol}!`
+    }
+}
